38d35d7c104043b52598626314b21c7689691899
Isak Bedard <isakbedard@gmail.com>
2019-11-28 15:05:18 -0500

    debut

---
+++ IsakBedardDemineur.X/IsakBedardDemineur.X/build/Mini_board_config/debug/_ext/2113287972/Lcd4Lignes.p1.d
@@ -0,0 +1,4 @@
+ build/Mini_board_config/debug/_ext/2113287972/Lcd4Lignes.d  \
+ build/Mini_board_config/debug/_ext/2113287972/Lcd4Lignes.p1:  \
+ ../../../Downloads/Lcd4Lignes.c  \
+../../../Downloads/Lcd4Lignes.h 
---
+++ IsakBedardDemineur.X/IsakBedardDemineur.X/main.c
@@ -0,0 +1,79 @@
+ /**
+ * @file main.c  
+ * @author Isak Bédard
+ * @date   28 novembre 2019
+ * @brief  Jeu : Démineur.
+  * 
+  * 
+ *
+ * @version 1.0
+ * Environnement:
+ *     Développement: MPLAB X IDE (version 5.25)
+ *     Compilateur: XC8 (version 2.10)
+ *     Matériel: Carte démo du Pickit3, PIC 18F45K20, écran LCD 4x20, power pack
+  * 5V/3V3, joystick XY avec bouton poussoir
+  */
+/****************** Liste des INCLUDES ****************************************/
+#include <xc.h>
+#include <stdbool.h>  // pour l'utilisation du type bool
+#include <conio.h>
+#include "Lcd4Lignes.h" // pour utiliser le fichier header lcd4Lignes.h
+#include "serie.h" //pour utiliser le fichier header serie.h
+#include <stdlib.h>
+/********************** CONSTANTES *******************************************/
+#define _XTAL_FREQ 1000000 //Constante utilisée par __delay_ms(x). Doit = fréq interne du uC
+#define NB_LIGNE 4  //afficheur LCD 4x20
+#define NB_COL 20
+#define AXE_X 7  //canal analogique de l'axe x de la manette
+#define AXE_Y 6
+#define PORT_SW PORTBbits.RB1 //sw de la manette
+#define TUILE 1 //caractère cgram d'une tuile
+#define MINE 2 //caractère cgram d'une mine
+/********************** PROTOTYPES *******************************************/
+void initialisation(void);
+
+/****************** VARIABLES GLOBALES ****************************************/
+ char m_tabVue[NB_LIGNE][NB_COL+1]; //Tableau des caractères affichés au LCD
+ char m_tabMines[NB_LIGNE][NB_COL+1];//Tableau contenant les mines, les espaces et les chiffres
+/******************** PROGRAMME PRINCPAL **************************************/
+void main(void)
+{
+    initialisation();
+}
+
+/**
+ * @brief Fait l'initialisation des différents registres et variables.
+ * @param Aucun
+ * @return Aucun
+ */
+void initialisation(void)
+{
+    TRISD = 0; //Tout le port D en sortie
+    ANSELH = 0;  // RB0 à RB4 en mode digital. Sur 18F45K20 AN et PortB sont sur les memes broches
+    TRISB = 0xFF; //tout le port B en entree
+    ANSEL = 0;  // PORTA en mode digital. Sur 18F45K20 AN et PortA sont sur les memes broches
+    TRISA = 0; //tout le port A en sortie
+   
+    //Pour du vrai hasard, on doit rajouter ces lignes. 
+    //Ne fonctionne pas en mode simulateur.
+    T1CONbits.TMR1ON = 1;
+    srand(TMR1);
+   //Configuration du port analogique
+    ANSELbits.ANS7 = 1;  //A7 en mode analogique
+    ADCON0bits.ADON = 1; //Convertisseur AN à on
+	ADCON1 = 0; //Vref+ = VDD et Vref- = VSS
+    ADCON2bits.ADFM = 0; //Alignement à gauche des 10bits de la conversion (8 MSB dans ADRESH, 2 LSB à gauche dans ADRESL)
+    ADCON2bits.ACQT = 0;//7; //20 TAD (on laisse le max de temps au Chold du convertisseur AN pour se charger)
+    ADCON2bits.ADCS = 0;//6; //Fosc/64 (Fréquence pour la conversion la plus longue possible)
+    /**************Timer 0*****************/
+    T0CONbits.TMR0ON    = 1;
+    T0CONbits.T08BIT    = 0; // mode 16 bits
+    T0CONbits.T0CS      = 0;
+    T0CONbits.PSA       = 0; // prescaler enabled
+    T0CONbits.T0PS      = 0b010; // 1:8 pre-scaler
+    TMR0 = DELAI_TMR0;
+    INTCONbits.TMR0IE   = 1;  // timer 0 interrupt enable
+    INTCONbits.TMR0IF   = 0; // timer 0 interrupt flag
+    INTCONbits.PEIE = 1; //permet interruption des périphériques
+    INTCONbits.GIE = 1;  //interruptions globales permises
+}
