765964b21faa637d4936e4487037c2c82d4dd456
Isak Bedard <isakbedard@gmail.com>
2019-11-29 12:52:30 -0500

    canvas des fonctions

--- IsakBedardDemineur.X/IsakBedardDemineur.X/main.c
+++ IsakBedardDemineur.X/IsakBedardDemineur.X/main.c
@@ -31,7 +31,14 @@
 #define MINE 2 //caractère cgram d'une mine
 /********************** PROTOTYPES *******************************************/
 void initialisation(void);
-
+void initTabVue(void);
+void rempliMines(int nb);
+void metToucheCombien(void);
+char calculToucheCombien(int ligne, int colonne);
+void deplace(char* x, char* y);
+bool demine(char x, char y);
+void enleveTuilesAutour(char x, char y);
+bool gagne(int* pMines);
 /****************** VARIABLES GLOBALES ****************************************/
  char m_tabVue[NB_LIGNE][NB_COL+1]; //Tableau des caractères affichés au LCD
  char m_tabMines[NB_LIGNE][NB_COL+1];//Tableau contenant les mines, les espaces et les chiffres
@@ -65,4 +72,91 @@ void initialisation(void)
     ADCON2bits.ADFM = 0; //Alignement à gauche des 10bits de la conversion (8 MSB dans ADRESH, 2 LSB à gauche dans ADRESL)
     ADCON2bits.ACQT = 0;//7; //20 TAD (on laisse le max de temps au Chold du convertisseur AN pour se charger)
     ADCON2bits.ADCS = 0;//6; //Fosc/64 (Fréquence pour la conversion la plus longue possible)
+}
+
+/*
+ * @brief Rempli le tableau m_tabVue avec le caractère spécial (définie en CGRAM
+ *  du LCD) TUILE. Met un '\0' à la fin de chaque ligne pour faciliter affichage
+ *  avec lcd_putMessage().
+ * @param rien
+ * @return rien
+ */
+void initTabVue(void)
+{
+    
+}
+ 
+/*
+ * @brief Rempli le tableau m_tabMines d'un nombre (nb) de mines au hasard.
+ *  Les cases vides contiendront le code ascii d'un espace et les cases avec
+ *  mine contiendront le caractère MINE défini en CGRAM.
+ * @param int nb, le nombre de mines à mettre dans le tableau 
+ * @return rien
+ */
+void rempliMines(int nb)
+{
+    
+}
+/*
+ * @brief Rempli le tableau m_tabMines avec le nombre de mines que touche la case.
+ * Si une case touche à 3 mines, alors la méthode place le code ascii de 3 dans
+ * le tableau. Si la case ne touche à aucune mine, la méthode met le code
+ * ascii d'un espace.
+ * Cette méthode utilise calculToucheCombien(). 
+ * @param rien
+ * @return rien
+ */
+void metToucheCombien(void)
+{
+    
+}
+/*
+ * @brief Calcul à combien de mines touche la case. Cette méthode est appelée par metToucheCombien()
+ * @param int ligne, int colonne La position dans le tableau m_tabMines a vérifier
+ * @return char nombre. Le nombre de mines touchées par la case
+ */
+char calculToucheCombien(int ligne, int colonne)
+{
+    
+}
+/**
+ * @brief Si la manette est vers la droite ou la gauche, on déplace le curseur 
+ * d'une position (gauche, droite, bas et haut)
+ * @param char* x, char* y Les positions X et y  sur l'afficheur
+ * @return rien
+ */
+void deplace(char* x, char* y)
+{
+    
+}
+/*
+ * @brief Dévoile une tuile (case) de m_tabVue. 
+ * S'il y a une mine, retourne Faux. Sinon remplace la case et les cases autour
+ * par ce qu'il y a derrière les tuiles (m_tabMines).
+ * Utilise enleveTuileAutour().
+ * @param char x, char y Les positions X et y sur l'afficheur LCD
+ * @return faux s'il y avait une mine, vrai sinon
+ */
+bool demine(char x, char y)
+{
+    
+}
+/*
+ * @brief Dévoile les cases non minées autour de la tuile reçue en paramètre.
+ * Cette méthode est appelée par demine().
+ * @param char x, char y Les positions X et y sur l'afficheur LCD.
+ * @return rien
+ */
+void enleveTuilesAutour(char x, char y)
+ 
+/*
+ * @brief Vérifie si gagné. On a gagné quand le nombre de tuiles non dévoilées
+ * est égal au nombre de mines. On augmente de 1 le nombre de mines si on a 
+ * gagné.
+ * @param int* pMines. Le nombre de mine.
+ * @return vrai si gagné, faux sinon
+ */
+bool gagne(int* pMines)
+{
+    
 }
